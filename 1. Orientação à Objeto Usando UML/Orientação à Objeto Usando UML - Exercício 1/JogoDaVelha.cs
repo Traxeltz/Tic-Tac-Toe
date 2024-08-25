26c1db460cb5b3fa7014bd0429d1de01baed9aec
Felipe Anunciação <felipe.anunciacao@hotmail.com>
2024-08-24 21:50:44 -0300

    Adicionar arquivos de projeto.

---
+++ 1. Orientação à Objeto Usando UML/Orientação à Objeto Usando UML - Exercício 1/JogoDaVelha.cs
@@ -0,0 +1,276 @@
+﻿using System;
+
+namespace JogoDaVelha
+{
+    // Programa Principal
+    public class Jogo
+    {
+        public static void Main()
+        {
+            // Inserção de dados para o jogo
+            bool entradasValidas = false;
+            while (!entradasValidas) // Loop para reiniciar o programa em caso de erro
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("---JOGO DA VELHA---");
+
+                try
+                {
+                    Console.WriteLine("\nJogador 1, insira seu nome!");
+                    string nome1 = Console.ReadLine().Trim(); // Nome do jogador 1
+                    if (string.IsNullOrWhiteSpace(nome1)) // Caso o nome não esteja correto
+                    {
+                        throw new ArgumentException("O nome não pode ser vazio. Por favor, insira um nome.");
+                    }
+                    Console.WriteLine("Escolha seu símbolo! (X ou O)");
+                    char simbolo1 = char.Parse(Console.ReadLine().ToUpper().Trim()); // Símbolo do jogador 1
+                    if (simbolo1 != 'X' && simbolo1 != 'O') // Caso o símbolo não esteja correto
+                    {
+                        throw new ArgumentException("O símbolo inserido não é válido. Por favor, insira um dos símbolos adequados.");
+                    }
+                    Console.WriteLine("\nJogador 2, insira seu nome!");
+                    string nome2 = Console.ReadLine().Trim(); // Nome do jogador 2
+                    if (string.IsNullOrWhiteSpace(nome2)) // Caso o nome não esteja correto
+                    {
+                        throw new ArgumentException("O nome não pode ser vazio. Por favor, insira um nome.");
+                    }
+                    char simbolo2 = ' ';
+                    if (simbolo1 == 'X') // Se esse símbolo for escolhido, o outro jogador tem o contrário
+                    {
+                        simbolo2 = 'O';
+                    }
+                    else if (simbolo1 == 'O') // Se esse símbolo for escolhido, o outro jogador tem o contrário
+                    {
+                        simbolo2 = 'X';
+                    }
+
+                    // Inicialização do Jogo
+                    bool jogoFinalizado = false;
+                    int pontuacao1 = 0;
+                    int pontuacao2 = 0;
+                    while (!jogoFinalizado) // Início do loop de rodadas do jogo
+                    {
+                        Tabuleiro tabuleiro = new Tabuleiro();
+                        int linha;
+                        int coluna;
+
+                        // Início do Loop do Jogo
+                        while (true)
+                        {
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.White;
+                            tabuleiro.ExibirTabuleiro(); // Mostrar tabuleiro pela primeira vez
+
+                            // Caso o Jogador 1 tenha escolhido o X
+                            if (simbolo1 == 'X')
+                            {
+                                // Jogador 1 irá jogar
+                                do
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.WriteLine($"{nome1}, insira as coordenadas da sua jogada!");
+                                    Console.Write("Linha: ");
+                                    linha = int.Parse(Console.ReadLine().Trim());
+                                    Console.Write("Coluna: ");
+                                    coluna = int.Parse(Console.ReadLine().Trim());
+                                    if (tabuleiro.QuadradoPreenchido(linha, coluna)) // Verificar se a casa está vazia
+                                    {
+                                        Console.WriteLine("\nEsta casa já foi preenchida. Insira uma coordenada para uma casa vazia!\n");
+                                        continue;
+                                    }
+                                }
+                                while (tabuleiro.QuadradoPreenchido(linha, coluna));
+                                tabuleiro.FazerJogada(linha, coluna, simbolo1); // Fazer a jogada se for válida
+
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.White;
+                                tabuleiro.ExibirTabuleiro(); // Mostrar tabuleiro na situação atual
+
+                                if (tabuleiro.VerificarVitoria(simbolo1)) // Verificar se uma condição de vitória foi atingida
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.WriteLine($"{nome1} venceu o Jogo da Velha!");
+                                    pontuacao1++;
+                                    break;
+                                }
+                                if (tabuleiro.VerificarVelha()) // Verificar se não é mais possível vencer
+                                {
+                                    Console.WriteLine("Deu velha!\nNinguém venceu!");
+                                    break;
+                                }
+
+                                // Jogador 2 irá jogar
+                                do
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"{nome2}, insira as coordenadas da sua jogada!");
+                                    Console.Write("Linha: ");
+                                    linha = int.Parse(Console.ReadLine().Trim());
+                                    Console.Write("Coluna: ");
+                                    coluna = int.Parse(Console.ReadLine().Trim());
+                                    if (tabuleiro.QuadradoPreenchido(linha, coluna)) // Verificar se a casa está vazia
+                                    {
+                                        Console.WriteLine("\nEsta casa já foi preenchida. Insira uma coordenada para uma casa vazia!\n");
+                                        continue;
+                                    }
+                                }
+                                while (tabuleiro.QuadradoPreenchido(linha, coluna));
+                                tabuleiro.FazerJogada(linha, coluna, simbolo2); // Fazer a jogada se for válida
+
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.White;
+                                tabuleiro.ExibirTabuleiro(); // Mostrar tabuleiro na situação atual
+
+                                if (tabuleiro.VerificarVitoria(simbolo2)) // Verificar se uma condição de vitória foi atingida
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"{nome2} venceu o Jogo da Velha!");
+                                    pontuacao2++;
+                                    break;
+                                }
+                                if (tabuleiro.VerificarVelha()) // Verificar se não é mais possível vencer
+                                {
+                                    Console.WriteLine("Deu velha!\nNinguém venceu!");
+                                    break;
+                                }
+                            }
+
+                            // Caso o Jogador 2 tenha escolhido o X
+                            if (simbolo2 == 'X')
+                            {
+                                // Jogador 2 irá jogar
+                                do
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"{nome2}, insira as coordenadas da sua jogada!");
+                                    Console.Write("Linha: ");
+                                    linha = int.Parse(Console.ReadLine().Trim());
+                                    Console.Write("Coluna: ");
+                                    coluna = int.Parse(Console.ReadLine().Trim());
+                                    if (tabuleiro.QuadradoPreenchido(linha, coluna)) // Verificar se a casa está vazia
+                                    {
+                                        Console.WriteLine("\nEsta casa já foi preenchida. Insira uma coordenada para uma casa vazia!\n");
+                                        continue;
+                                    }
+                                }
+                                while (tabuleiro.QuadradoPreenchido(linha, coluna));
+                                tabuleiro.FazerJogada(linha, coluna, simbolo2); // Fazer a jogada se for válida
+
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.White;
+                                tabuleiro.ExibirTabuleiro(); // Mostrar tabuleiro na situação atual
+
+                                if (tabuleiro.VerificarVitoria(simbolo2)) // Verificar se uma condição de vitória foi atingida
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"{nome2} venceu o Jogo da Velha!");
+                                    pontuacao2++;
+                                    break;
+                                }
+                                if (tabuleiro.VerificarVelha()) // Verificar se não é mais possível vencer
+                                {
+                                    Console.WriteLine("Deu velha!\nNinguém venceu!");
+                                    break;
+                                }
+
+                                // Jogador 1 irá jogar
+                                do
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.WriteLine($"{nome1}, insira as coordenadas da sua jogada!");
+                                    Console.Write("Linha: ");
+                                    linha = int.Parse(Console.ReadLine().Trim());
+                                    Console.Write("Coluna: ");
+                                    coluna = int.Parse(Console.ReadLine().Trim());
+                                    if (tabuleiro.QuadradoPreenchido(linha, coluna)) // Verificar se a casa está vazia
+                                    {
+                                        Console.WriteLine("\nEsta casa já foi preenchida. Insira uma coordenada para uma casa vazia!\n");
+                                        continue;
+                                    }
+                                }
+                                while (tabuleiro.QuadradoPreenchido(linha, coluna));
+                                tabuleiro.FazerJogada(linha, coluna, simbolo1); // Fazer a jogada se for válida
+
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.White;
+                                tabuleiro.ExibirTabuleiro(); // Mostrar tabuleiro na situação atual
+
+                                if (tabuleiro.VerificarVitoria(simbolo1)) // Verificar se uma condição de vitória foi atingida
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.WriteLine($"{nome1} venceu o Jogo da Velha!");
+                                    pontuacao1++;
+                                    break;
+                                }
+                                if (tabuleiro.VerificarVelha()) // Verificar se não é mais possível vencer
+                                {
+                                    Console.WriteLine("Deu velha!\nNinguém venceu!");
+                                    break;
+                                }
+                            }
+                        }
+                        Console.WriteLine($"\n\t{pontuacao1} - {pontuacao2}");
+                        char Opcao = ' ';
+                        while (Opcao != 'S' && Opcao != 'N')
+                        {
+                            Console.Write("\nDeseja jogar mais uma partida? (S/N): ");
+                            Opcao = char.Parse(Console.ReadLine().ToUpper().Trim());
+                            switch (Opcao)
+                            {
+                                case 'S':
+                                    break;
+                                case 'N':
+                                    jogoFinalizado = true;
+                                    break;
+                                default:
+                                    Console.WriteLine("Opção inválida!");
+                                    break;
+                            }
+                        }
+                    }
+                    // Final do Loop do Jogo
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\nObrigado por jogar o Jogo da Velha!");
+                    entradasValidas = true;
+                }
+                catch (ArgumentException Erro)
+                {
+                    Console.ResetColor();
+                    Console.WriteLine($"\n{Erro.Message}");
+                    Console.WriteLine("Pressione qualquer tecla para reiniciar o programa.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    GC.Collect();
+                    continue;
+                }
+                catch (FormatException)
+                {
+                    Console.ResetColor();
+                    Console.WriteLine("\nO valor inserido não é apropriado. Por favor, insira valores apropriados.");
+                    Console.WriteLine("Pressione qualquer tecla para reiniciar o programa.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    GC.Collect();
+                    continue;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.ResetColor();
+                    Console.WriteLine("\nEste valor para a coordenada não existe. Por favor, insira valores apropriados.");
+                    Console.WriteLine("Pressione qualquer tecla para reiniciar o programa.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    GC.Collect();
+                    continue;
+                }
+            }
+
+            // Encerramento do Código
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Este programa chegou ao final. Para fechá-lo, pressione qualquer tecla.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1. Orientação à Objeto Usando UML/Orientação à Objeto Usando UML - Exercício 1/Tabuleiro.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace JogoDaVelha
+{
+    // Programa de Operações do Tabuleiro
+    public class Tabuleiro
+    {
+        // Leitura Interna do Tabuleiro
+        private readonly char[,] quadrados;
+
+        // Construção do Tabuleiro
+        public Tabuleiro()
+        {
+            quadrados = new char[3, 3];
+            InicializarTabuleiro();
+        }
+        
+        // Inicialização do Tabuleiro com Caracteres Vazios
+        public void InicializarTabuleiro()
+        {
+            for (int i = 0; i < quadrados.GetLength(0); i++)
+            {
+                for (int j = 0; j < quadrados.GetLength(1); j++)
+                {
+                    quadrados[i, j] = ' ';
+                }
+            }
+        }
+
+        // Exibição do Tabuleiro na Tela
+        public void ExibirTabuleiro()
+        {
+            for (int i = 0; i < quadrados.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < quadrados.GetLength(1); j++)
+                {
+                    Console.Write(" " + quadrados[i, j] + " ");
+                    if (j < quadrados.GetLength(1) - 1)
+                    {
+                        Console.Write('|');
+                    }
+                }
+                Console.WriteLine();
+                if (i < quadrados.GetLength(0) - 1)
+                {
+                    Console.Write("-----------");
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        // Verifica se uma casa do tabuleiro está preenchida
+        public bool QuadradoPreenchido(int linha, int coluna)
+        {
+            return quadrados[linha - 1, coluna - 1] != ' ';
+        }
+
+        // Realização de uma jogada por um jogador
+        public void FazerJogada(int linha, int coluna, char simbolo)
+        {
+            quadrados[linha - 1, coluna - 1] = simbolo;
+        }
+
+        // Verifica se algum jogador venceu o jogo
+        public bool VerificarVitoria(char simbolo)
+        {
+            // Verificação de Linhas
+            for (int i = 0; i < quadrados.GetLength(0); i++)
+            {
+                if (quadrados[i, 0] == simbolo && quadrados[i, 1] == simbolo && quadrados[i, 2] == simbolo)
+                {
+                    return true; // Linha completa
+                }
+            }
+
+            // Verificação de Colunas
+            for (int i = 0; i < quadrados.GetLength(1); i++)
+            {
+                if (quadrados[0, i] == simbolo && quadrados[1, i] == simbolo && quadrados[2, i] == simbolo)
+                {
+                    return true; // Coluna completa
+                }
+            }
+
+            // Verificação da Diagonal Principal
+            if (quadrados[0, 0] == simbolo && quadrados[1, 1] == simbolo && quadrados[2, 2] == simbolo)
+            {
+                return true; // Diagonal principal completa
+            }
+
+            // Verificação da Diagonal Secundária
+            if (quadrados[0, 2] == simbolo && quadrados[1, 1] == simbolo && quadrados[2, 0] == simbolo)
+            {
+                return true; // Diagonal secundária completa
+            }
+
+            return false; // Ninguém venceu ainda
+        }
+
+        // Verifica se não há mais possibilidade de vencer
+        public bool VerificarVelha()
+        {
+            int quadradosVazios = 0;
+            foreach (char quadrado in quadrados) // Verificar cada casa do tabuleiro
+            {
+                if (quadrado == ' ') // Se for vazio
+                {
+                    quadradosVazios++;
+                }
+            }
+            if (quadradosVazios == 0) // Caso todas as casas estejam preenchidas
+            {
+                return true; // Deu velha
+            }
+            return false; // Ainda há como jogar
+        }
+
+        // Destruição do Tabuleiro ao Final
+        ~Tabuleiro() { }
+    }
+}
